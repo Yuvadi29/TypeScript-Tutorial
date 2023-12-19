6be6d40ca9bd75c3f4337c8f910231cc2fa82438
Yuvadi29 <adityatrivedi612@gmail.com>
2023-12-19 19:48:24 +0530

    New Note Functionality

--- Project-01/Notes-App/src/App.tsx
+++ Project-01/Notes-App/src/App.tsx
@@ -1,13 +1,37 @@
-import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
+import NewNote from "./components/NewNote";
+
+
+type Note = {
+  id: string;
+} & NoteData; //Add noteData to end of the Note type
+ 
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+}
+
+export type Tag = {
+  id: string;
+  label: string;
+}
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/new" element={<h1>New</h1>} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   )
 }
 
