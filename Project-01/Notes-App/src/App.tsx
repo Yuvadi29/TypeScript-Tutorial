51da45abb1ae35e7632f0a76043faf0b04aa112d
Yuvadi29 <adityatrivedi612@gmail.com>
2023-12-19 20:06:11 +0530

    useLocalStorage Hook Creation, Note Creation Logic

--- Project-01/Notes-App/src/App.tsx
+++ Project-01/Notes-App/src/App.tsx
@@ -2,12 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import NewNote from "./components/NewNote";
+import { useMemo, useState } from "react";
+import useLocalStorage from "./useLocalStorage";
+import { v4 as uuidV4 } from "uuid";
 
 
 type Note = {
   id: string;
 } & NoteData; //Add noteData to end of the Note type
- 
+
 export type NoteData = {
   title: string;
   markdown: string;
@@ -19,7 +22,38 @@ export type Tag = {
   label: string;
 }
 
+export type RawNote = {
+  id: string,
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+}
+
 const App = () => {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<RawNote[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return {
+        ...note,
+        tags: tags.filter(tag => note.tagIds.includes(tag.id))
+      }
+    })
+  }, [notes, tags]);
+
+  const createNote = ({ tags, ...data }: NoteData) => {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+      ]
+    })
+  }
+
   return (
     <Container className="my-4">
       <Routes>
