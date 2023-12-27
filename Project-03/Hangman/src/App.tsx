ba13e554c0449553705b4715592fcc39cf842de7
Yuvadi29 <adityatrivedi612@gmail.com>
2023-12-27 13:27:47 +0530

    Display Random Words from Json File

--- Project-03/Hangman/src/App.tsx
+++ Project-03/Hangman/src/App.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react"
+import { useState } from "react";
+import words from './wordList.json';
 
 const App = () => {
-  const [wordToGuess, setWordToGuess] = useState("Test");
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  });
+
+  console.log(wordToGuess);
+  
   return (
-    <div>App</div>
+    <div>{wordToGuess}</div>
   )
 }
 
