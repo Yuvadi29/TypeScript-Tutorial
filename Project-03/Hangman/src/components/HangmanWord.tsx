8164039a6951b0c9ce910b924462c0870d096ae8
Yuvadi29 <adityatrivedi612@gmail.com>
2023-12-28 07:14:21 +0530

    Word Guessing with Blanks

--- Project-03/Hangman/src/components/HangmanWord.tsx
+++ Project-03/Hangman/src/components/HangmanWord.tsx
@@ -1,8 +1,17 @@
 
 const HangmanWord = () => {
-  return (
-    <div>HangmanWord</div>
-  )
+    const word = "test";
+    const guessedLetters = ["t"];
+
+    return (
+        <div style={{ display: "flex", gap: "0.25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace" }}>
+            {word.split("").map((letter, index) => (
+                <span style={{ borderBottom: ".1em solid black" }} key={index}>
+                    <span style={{ visibility: guessedLetters.includes(letter) ? "visible" : "hidden" }}>{letter}</span>
+                </span>
+            ))}
+        </div>
+    )
 }
 
 export default HangmanWord
