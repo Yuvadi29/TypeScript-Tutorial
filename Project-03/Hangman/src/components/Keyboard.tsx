b74b0dd7e1dd6d7c0baf8a90301184f49b10ff79
Yuvadi29 <adityatrivedi612@gmail.com>
2023-12-28 07:16:12 +0530

    Added Keyboard

--- Project-03/Hangman/src/components/Keyboard.tsx
+++ Project-03/Hangman/src/components/Keyboard.tsx
@@ -1,8 +1,52 @@
+const KEYS = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
 
 const Keyboard = () => {
-  return (
-    <div>Keyboard</div>
-  )
+    return (
+        <div>
+            {KEYS.map(key => {
+                // const isActive = activeLetters.includes(key)
+                // const isInactive = inactiveLetters.includes(key)
+                return (
+                    <button
+                        // onClick={() => addGuessedLetter(key)}
+                        // className={`${styles.btn} ${isActive ? styles.active : ""} ${isInactive ? styles.inactive : ""
+                            // }`}
+                        // disabled={isInactive || isActive || disabled}
+                        key={key}
+                    >
+                        {key}
+                    </button>
+                )
+            })}
+        </div>
+    )
 }
 
 export default Keyboard
