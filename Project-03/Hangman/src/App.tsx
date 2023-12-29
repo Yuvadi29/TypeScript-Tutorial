ddf59659a261d3565047165cb327443e9670366b
Yuvadi29 <adityatrivedi612@gmail.com>
2023-12-29 10:20:34 +0530

    Word Logic Completed

--- Project-03/Hangman/src/App.tsx
+++ Project-03/Hangman/src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
       </div>
 
       <HangmanDrawing numberOfGuesses={inCorrectLetters.length} />
-      <HangmanWord />
+      <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
       <div style={{ alignSelf: "stretch" }}>
         <Keyboard />
       </div>
--- Project-03/Hangman/src/components/HangmanWord.tsx
+++ Project-03/Hangman/src/components/HangmanWord.tsx
@@ -1,11 +1,14 @@
 
-const HangmanWord = () => {
-    const word = "test";
-    const guessedLetters = ["t"];
+type HangmanWordProps = {
+    guessedLetters: string[],
+    wordToGuess: string
+}
+
+const HangmanWord = ({ guessedLetters, wordToGuess }: HangmanWordProps) => {
 
     return (
         <div style={{ display: "flex", gap: "0.25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace" }}>
-            {word.split("").map((letter, index) => (
+            {wordToGuess.split("").map((letter, index) => (
                 <span style={{ borderBottom: ".1em solid black" }} key={index}>
                     <span style={{ visibility: guessedLetters.includes(letter) ? "visible" : "hidden" }}>{letter}</span>
                 </span>
